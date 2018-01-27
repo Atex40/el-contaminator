0ae92738120e3f3209647fd0c9ace57a7ee4f6c6
MSI\Lucas <lucasblon@orange.fr>
2018-01-27 02:00:18 +0100

    scripts

---
+++ Assets/Scripts/S_Pourcent_Level1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class S_PrctMap1 : MonoBehaviour {
+
+	private float TotalConta;
+	private float TotalaConta = 25;
+	public static float pourcentageMap1;
+	public Text pourcentage;
+
+
+	private static S_PrctMap1 instance ;
+    public static S_PrctMap1 Instance () 
+    {
+        return instance;
+    }
+		void Awake ()
+    {
+        if (instance != null)
+        {
+            Destroy (gameObject);
+        }
+        else 
+        {
+            instance = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CalculPrct();
+		AffichagePrct ();
+		
+	}
+
+	public void CalculPrct ()
+	{
+		TotalConta = GameObject.FindGameObjectsWithTag("mort").Length;
+		pourcentageMap1 = TotalConta * 100 / TotalaConta;
+	}
+
+	void AffichagePrct ()
+	{
+		pourcentage.text = pourcentageMap1.ToString();
+	}
+}
---
+++ Assets/Scripts/S_RayCast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_Contamination : MonoBehaviour {
+
+	public float rayLenght;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 myray = Camera.main.transform.forward;
+		RaycastHit hit;
+		Debug.DrawRay(transform.position, myray * 20, Color.green);
+
+		if (Physics.Raycast(Camera.main.transform.position, myray, out hit, rayLenght))
+		{
+			if(hit.collider.gameObject.CompareTag ("CUBASS"))
+			{
+				Debug.Log (hit.collider.name);
+
+				if (Input.GetKeyDown("e"))
+				{
+					hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
+					hit.collider.gameObject.transform.tag = "mort";
+				}
+			}
+		}		
+	}
+}
